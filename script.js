cc1e4ff7faf96baf5de493900bdf76e4721ab8cc
danielquerrey <daniel.q@tamu.edu>
2023-09-09 06:39:26 -0500

    Basic Functions Implemented
    Added vanilla style and function.

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+
+function startGame(e) {
+    let num_boxes = Number(prompt(" Enter number of grid squares (A * A) "));
+    if (!num_boxes) return console.log("Wrong input"); // type check HERE
+    console.log(num_boxes);
+    // if (Object.is(document.querySelector('#box-array').firstElementChild, null)) return console.log('no child');
+    let boxArray = document.querySelector('#box-array');
+    if (!(boxArray.hasChildNodes())) { console.log('no child'); } else {
+        while (boxArray) {
+            if (boxArray.firstElementChild) { boxArray.firstElementChild.remove(); }
+            else break;
+        }
+    }
+
+    for (let i = 0; i < num_boxes; i++) {
+        let row = document.createElement('div')
+        row.id = 'row';
+        for (let j = 0; j < num_boxes; j++) {
+            let cell = document.createElement('div');
+            cell.id = 'cell';
+            // console.log(cell);
+            row.appendChild(cell);
+        }
+        let row_sel = document.querySelector('#box-array');
+        console.log(row_sel);
+        row_sel.appendChild(row);
+
+    }
+
+}
+
+
+let startButton = document.querySelector('#header-btn');
+startButton.addEventListener('click', startGame);
+
